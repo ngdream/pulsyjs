4e0a7a0441ada17ba960f0e05893a81052943698
ngdream <ngdream1953@gmail.com>
2024-08-26 15:26:23 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pulsyjs.github.io',
+  site: 'https://ngdream.github.io',
   base : "pulsyjs",
   markdown: {
     smartypants: true,
